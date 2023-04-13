6e674a934e1456b9ff1d197106a504598618d9ad
mackshirilla <130690025+mackshirilla@users.noreply.github.com>
2023-04-13 15:55:50 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,20 @@
-import { setCookie } from './cookies.js'; // Import the setCookie function from cookies.js
+// Import the setCookie function from cookies.js
+import { setCookie } from './cookies.js';
 
+/**
+ * Function to validate an email address
+ * @param {string} email - The email address to validate
+ * @returns {boolean} true if the email is valid, false otherwise
+ */
 function validateEmail(email) {
   const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
   return emailRegex.test(email);
 }
 
+/**
+ * Function to handle email input changes
+ * @param {Object} event - The input change event
+ */
 function handleEmailInput(event) {
   const emailInput = event.target;
   const email = emailInput.value;
@@ -20,6 +30,10 @@ function handleEmailInput(event) {
   }
 }
 
+/**
+ * Function to handle the form submit event
+ * @param {Object} event - The form submit event
+ */
 async function handleSignupFormSubmit(event) {
   event.preventDefault();
   event.stopImmediatePropagation();
@@ -33,55 +47,4 @@ async function handleSignupFormSubmit(event) {
   const submitErrorMessage = document.querySelector('[w-el="submitErrorMessage"]');
   const emailErrorMessage = document.querySelector('[w-el="emailErrorMessage"]');
 
-  // Validate email
-  if (!email || !validateEmail(email)) {
-    emailErrorMessage.style.display = 'block';
-    emailErrorMessage.textContent = 'Please enter a valid email address';
-    emailInput.style.boxShadow = '0 0 0 1px #ff3b30';
-    return;
-  } else {
-    emailErrorMessage.style.display = 'none';
-    emailInput.style.boxShadow = '';
-  }
-
-  loadingElement.style.display = 'block';
-  submitErrorMessage.style.display = 'none';
-
-  try {
-    const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:uf0rJHld/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email,
-        firstName,
-        lastName,
-      }),
-    });
-
-    const responseData = await response.json();
-
-    if (response.ok) {
-      // Store the token in a cookie before navigating to the dashboard
-      setCookie('authToken', responseData.authToken, 1);
-      window.location.assign('/my-dashboard');
-    } else {
-      submitErrorMessage.style.display = 'block';
-      submitErrorMessage.textContent = responseData.message || 'User registration failed';
-      console.error('User registration failed:', responseData);
-    }
-  } catch (error) {
-    submitErrorMessage.style.display = 'block';
-    submitErrorMessage.textContent = 'An error occurred during user registration';
-    console.error('An error occurred during user registration:', error);
-  } finally {
-    loadingElement.style.display = 'none';
-  }
-}
-
-// Add event listeners to the form and email input
-const signupForm = document.getElementById('wf-form-Form');
-const emailInput = document.getElementById('email');
-signupForm.addEventListener('submit', handleSignupFormSubmit);
-emailInput.addEventListener('input', handleEmailInput);
+ 
