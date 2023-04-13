414399dc2d5e5979d77a4e67b59d046b60d02b0a
mackshirilla <130690025+mackshirilla@users.noreply.github.com>
2023-04-13 15:55:19 -0400

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,24 +1,42 @@
+/**
+ * Function to set a cookie
+ * @param {string} name - The name of the cookie
+ * @param {string} value - The value of the cookie
+ * @param {number} days - The number of days the cookie should be stored for
+ */
 function setCookie(name, value, days) {
-  let expires = "";
+  let expires = '';
   if (days) {
     const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
   }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  document.cookie = name + '=' + (value || '')  + expires + '; path=/';
 }
 
+/**
+ * Function to get the value of a cookie
+ * @param {string} name - The name of the cookie
+ * @returns {string} The value of the cookie
+ */
 function getCookie(name) {
-  const nameEQ = name + "=";
-  const ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
+  const nameEQ = name + '=';
+  const ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
     if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
   }
   return null;
 }
 
-function eraseCookie(name) {
-  document.cookie = name + "=; Max-Age=-99999999;";  
+/**
+ * Function to delete a cookie
+ * @param {string} name - The name of the cookie
+ */
+function eraseCookie(name) {   
+  document.cookie = name + '=; Max-Age=-99999999;';  
 }
+
+// Export the functions to be used in other files
+export { setCookie, getCookie, eraseCookie };
